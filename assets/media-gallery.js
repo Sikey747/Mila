71110f3b3da6a8be80bcee01701a524f74a7ce58
sikey747 <Sikey747@gmail.com>
2024-04-03 19:51:00 +0300

    add color alt filter image add slider in product add lihgtbox and fix some problem

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -1,6 +1,6 @@
-if (!customElements.get('media-gallery')) {
+if (!customElements.get("media-gallery")) {
   customElements.define(
-    'media-gallery',
+    "media-gallery",
     class MediaGallery extends HTMLElement {
       constructor() {
         super();
@@ -9,16 +9,23 @@ if (!customElements.get('media-gallery')) {
           viewer: this.querySelector('[id^="GalleryViewer"]'),
           thumbnails: this.querySelector('[id^="GalleryThumbnails"]'),
         };
-        this.mql = window.matchMedia('(min-width: 750px)');
+        this.mql = window.matchMedia("(min-width: 750px)");
         if (!this.elements.thumbnails) return;
 
-        this.elements.viewer.addEventListener('slideChanged', debounce(this.onSlideChanged.bind(this), 500));
-        this.elements.thumbnails.querySelectorAll('[data-target]').forEach((mediaToSwitch) => {
+        this.elements.viewer.addEventListener(
+          "slideChanged",
+          debounce(this.onSlideChanged.bind(this), 500)
+        );
+        this.elements.thumbnails.querySelectorAll("[data-target]").forEach((mediaToSwitch) => {
           mediaToSwitch
-            .querySelector('button')
-            .addEventListener('click', this.setActiveMedia.bind(this, mediaToSwitch.dataset.target, false));
+            .querySelector("button")
+            .addEventListener(
+              "click",
+              this.setActiveMedia.bind(this, mediaToSwitch.dataset.target, false)
+            );
         });
-        if (this.dataset.desktopLayout.includes('thumbnail') && this.mql.matches) this.removeListSemantic();
+        if (this.dataset.desktopLayout.includes("thumbnail") && this.mql.matches)
+          this.removeListSemantic();
       }
 
       onSlideChanged(event) {
@@ -30,15 +37,17 @@ if (!customElements.get('media-gallery')) {
 
       setActiveMedia(mediaId, prepend) {
         const activeMedia = this.elements.viewer.querySelector(`[data-media-id="${mediaId}"]`);
-        this.elements.viewer.querySelectorAll('[data-media-id]').forEach((element) => {
-          element.classList.remove('is-active');
+        this.elements.viewer.querySelectorAll("[data-media-id]").forEach((element) => {
+          element.classList.remove("is-active");
         });
-        activeMedia.classList.add('is-active');
+        activeMedia.classList.add("is-active");
 
         if (prepend) {
           activeMedia.parentElement.prepend(activeMedia);
           if (this.elements.thumbnails) {
-            const activeThumbnail = this.elements.thumbnails.querySelector(`[data-target="${mediaId}"]`);
+            const activeThumbnail = this.elements.thumbnails.querySelector(
+              `[data-target="${mediaId}"]`
+            );
             activeThumbnail.parentElement.prepend(activeThumbnail);
           }
           if (this.elements.viewer.slider) this.elements.viewer.resetPages();
@@ -53,12 +62,14 @@ if (!customElements.get('media-gallery')) {
           // Don't scroll if the image is already in view
           if (activeMediaRect.top > -0.5) return;
           const top = activeMediaRect.top + window.scrollY;
-          window.scrollTo({ top: top, behavior: 'smooth' });
+          window.scrollTo({ top: top, behavior: "smooth" });
         });
         this.playActiveMedia(activeMedia);
 
         if (!this.elements.thumbnails) return;
-        const activeThumbnail = this.elements.thumbnails.querySelector(`[data-target="${mediaId}"]`);
+        const activeThumbnail = this.elements.thumbnails.querySelector(
+          `[data-target="${mediaId}"]`
+        );
         this.setActiveThumbnail(activeThumbnail);
         this.announceLiveRegion(activeMedia, activeThumbnail.dataset.mediaPosition);
       }
@@ -67,22 +78,25 @@ if (!customElements.get('media-gallery')) {
         if (!this.elements.thumbnails || !thumbnail) return;
 
         this.elements.thumbnails
-          .querySelectorAll('button')
-          .forEach((element) => element.removeAttribute('aria-current'));
-        thumbnail.querySelector('button').setAttribute('aria-current', true);
+          .querySelectorAll("button")
+          .forEach((element) => element.removeAttribute("aria-current"));
+        thumbnail.querySelector("button").setAttribute("aria-current", true);
         if (this.elements.thumbnails.isSlideVisible(thumbnail, 10)) return;
 
         this.elements.thumbnails.slider.scrollTo({ left: thumbnail.offsetLeft });
       }
 
       announceLiveRegion(activeItem, position) {
-        const image = activeItem.querySelector('.product__modal-opener--image img');
+        const image = activeItem.querySelector(".product__modal-opener--image img");
         if (!image) return;
         image.onload = () => {
-          this.elements.liveRegion.setAttribute('aria-hidden', false);
-          this.elements.liveRegion.innerHTML = window.accessibilityStrings.imageAvailable.replace('[index]', position);
+          this.elements.liveRegion.setAttribute("aria-hidden", false);
+          this.elements.liveRegion.innerHTML = window.accessibilityStrings.imageAvailable.replace(
+            "[index]",
+            position
+          );
           setTimeout(() => {
-            this.elements.liveRegion.setAttribute('aria-hidden', true);
+            this.elements.liveRegion.setAttribute("aria-hidden", true);
           }, 2000);
         };
         image.src = image.src;
@@ -90,20 +104,22 @@ if (!customElements.get('media-gallery')) {
 
       playActiveMedia(activeItem) {
         window.pauseAllMedia();
-        const deferredMedia = activeItem.querySelector('.deferred-media');
+        const deferredMedia = activeItem.querySelector(".deferred-media");
         if (deferredMedia) deferredMedia.loadContent(false);
       }
 
       preventStickyHeader() {
-        this.stickyHeader = this.stickyHeader || document.querySelector('sticky-header');
+        this.stickyHeader = this.stickyHeader || document.querySelector("sticky-header");
         if (!this.stickyHeader) return;
-        this.stickyHeader.dispatchEvent(new Event('preventHeaderReveal'));
+        this.stickyHeader.dispatchEvent(new Event("preventHeaderReveal"));
       }
 
       removeListSemantic() {
         if (!this.elements.viewer.slider) return;
-        this.elements.viewer.slider.setAttribute('role', 'presentation');
-        this.elements.viewer.sliderItems.forEach((slide) => slide.setAttribute('role', 'presentation'));
+        this.elements.viewer.slider.setAttribute("role", "presentation");
+        this.elements.viewer.sliderItems.forEach((slide) =>
+          slide.setAttribute("role", "presentation")
+        );
       }
     }
   );
